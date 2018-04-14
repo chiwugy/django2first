dfa6716d9b0727268715eb18f6f818e4042d6dfa
chiwugy <15189568371@163.com>
2018-04-14 17:59:36 +0800

    add blogs/details.html and revise some typos

--- blogs/views.py
+++ blogs/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 
-#from .models import User
+# from .models import User
 
-#from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
 
 def index(request):
-    return render(request,'blogs/index.html')
+    return render(request, 'blogs/index.html')
+
 
 def lists(request):
     return render(request, 'blogs/list.html')
 
+
 def details(request):
-    return render(request, 'blogs/list.html')
+    return render(request, 'blogs/details.html')
